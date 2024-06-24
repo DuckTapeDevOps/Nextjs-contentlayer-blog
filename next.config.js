a21f83aee535753d1f5b7c37d69e671a62e5eb9c
DuckTapeDevOps <nic@ducktapedevops.com>
2024-06-24 10:57:11 -0400

    Added static html output option for deployment to S3

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        loader: 'default',
+        path: '/',
+    },
+}
 
 module.exports = nextConfig
